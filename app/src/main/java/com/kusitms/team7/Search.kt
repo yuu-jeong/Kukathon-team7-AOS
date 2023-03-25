023b00d5b5878e4a408c8c8d73ec62a39567444b
ShinKing <ssk775.@naver.com>
2023-03-26 02:21:05 +0900

    feat

--- app/src/main/java/com/kusitms/team7/Search.kt
+++ app/src/main/java/com/kusitms/team7/Search.kt
@@ -9,11 +9,7 @@ class Search : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
-        var search_result = findViewById<TextView>(R.id.search_result)
 
-        var data = intent.getStringExtra("search_text")
-
-        search_result.text = data
 
 
 
